58ef470126400e8f956fda593fa4604a9f96d2a0
iidakyouhei <kunekune0803@gmail.com>
2020-08-01 15:02:08 +0900

    [WIP]DB設計#2

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
   has_many :grops_user
-  has_many :message, through: :grops_user
+  has_many :message
+  has_many :user, through: :grops_user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :grops_user
-  has_many :message, through: :grops_user
+  has_many :message
+  has_many :group, through: :grops_user
 end
